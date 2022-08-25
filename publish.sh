d1d365898c34a49cdf626fa4282d9fc4c8342f17
Jalaluddin <jalaluddin.almursyidy@ikonsultan.com>
2022-08-25 13:39:14 +0700

    Cleaning up unused sh scripts

--- publish.sh
+++
@@ -1,6 +0,0 @@
-echo "Starting to publish docker image.."
-LOCAL_IMAGE = 'registry-sistem-akademik:0.0.1'
-REPO_IMAGE = 'jalalazhmatkhan/sistem-akademik'
-DOCKER_IMAGE=jalalazhmatkhan/sistem-akademik
-docker tag $LOCAL_IMAGE $REPO_IMAGE
-docker push $REPO_IMAGE
